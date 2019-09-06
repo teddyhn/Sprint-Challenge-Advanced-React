b635eb3a51ecf6696e132197d5261d426f20d456
teddyhn <tngo97@gmail.com>
2019-09-06 08:44:01 -0700

    App displaying data

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,48 @@
 import React from 'react';
+import axios from 'axios';
+import Card from 'react-bootstrap/Card';
+import CardDeck from 'react-bootstrap/CardDeck';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      data: []
+    };
+  }
+
+  componentDidMount() {
+    this.getData();
+  }
+
+  getData() {
+    axios
+      .get('http://localhost:5000/api/players')
+      // .then(response => console.log(response.data))
+      .then(response => this.setState({ data: response.data }))
+  }
+
+  render() {
+    const { data } = this.state;
+    
+    return (
+      <div className="App">
+        {data.map(item => (
+          <Card>
+            <Card.Body>
+              <Card.Title>{item.name}</Card.Title>
+              <Card.Text>
+                <p>{item.country}</p>
+                <p>Number of searches: {item.searches}</p>
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
